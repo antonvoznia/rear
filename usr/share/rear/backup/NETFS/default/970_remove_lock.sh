e7b510651462439f635cc2f4d17e96cffc9b861e
antonvoznia <antoncty@gmail.com>
2022-12-01 23:53:00 +0100

    severity error, 2625

--- usr/share/rear/backup/NETFS/default/970_remove_lock.sh
+++ usr/share/rear/backup/NETFS/default/970_remove_lock.sh
@@ -1,7 +1,8 @@
 # remove the lockfile
-local scheme=$( url_scheme $BACKUP_URL )
-local path=$( url_path $BACKUP_URL )
-local opath=$( backup_path $scheme $path )
+url="$( echo $stage | tr '[:lower:]' '[:upper:]')_URL"
+local scheme=$(url_scheme ${!url})
+local path=$(url_path ${!url})
+local opath=$(backup_path $scheme $path)
 
 # if $opath is empty return silently (e.g. scheme tape)
 [ -z "$opath" ] && return 0
