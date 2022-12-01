1896ff281334a54d1bce78a428229f2cf4be388d
antonvoznia <antoncty@gmail.com>
2022-12-01 23:53:27 +0100

    severity info, 2625

--- usr/share/rear/backup/NETFS/default/200_make_prefix_dir.sh
+++ usr/share/rear/backup/NETFS/default/200_make_prefix_dir.sh
@@ -2,14 +2,13 @@
 # to $HOSTNAME
 
 # do not do this for tapes and special attention for file:///path
-local scheme=$( url_scheme $BACKUP_URL )
-local path=$( url_path $BACKUP_URL )
-local opath=$( backup_path $scheme $path )
+url="$( echo $stage | tr '[:lower:]' '[:upper:]')_URL"
+local scheme=$(url_scheme ${!url})
+local path=$(url_path ${!url})
+local opath=$(backup_path $scheme $path)
 
 # if $opath is empty return silently (e.g. scheme tape)
 [ -z "$opath" ] && return 0
 
-mkdir -p $v -m0750 "${opath}" && return
-
-# A failure to create the $NETFS_PREFIX sub-directory is fatal:
-Error "Failed to create '$opath' directory for BACKUP_URL=$BACKUP_URL"
+mkdir -p $v -m0750 "${opath}" >&2
+StopIfError "Could not mkdir '${opath}'"
